c53fea2ce679bc1d79279b1ab28fa2281c06b833
Matt Anthem <mattrj.lema@gmail.com>
2025-11-13 21:41:57 +0300

    Removed unused files

--- src/pages/HistoricalExplorer.tsx
+++ src/pages/HistoricalExplorer.tsx
@@ -3,17 +3,88 @@ import VpHistChart from '../features/history_charts/components/VpHistChart';
 import VptsHistChart from '../features/history_charts/components/VptsHistChart';
 import VtipHistChart from '../features/history_charts/components/VtipHistChart';
 import HistoryMap from '../features/history_map/HistoryMap';
-import DashboardLayout from '../shared/layouts/DashboardLayout';
+import MainLayout from '../shared/layouts/MainLayout';
 
 const HistoricalExplorer = () => {
   return (
+    <MainLayout className="w-full min-h-screen">
+      
+      <div 
+        className={`
+          lg:h-full lg:w-full
+          lg:px-4 lg:py-8 py-4
+          flex flex-col  
+          lg:grid lg:grid-rows-[4fr_2fr] gap-2  
+        `}
+      >
 
-    <DashboardLayout
-      map={<HistoryMap/>}
-      vpChart={<VpHistChart/>}
-      vtipChart={<VtipHistChart/>}
-      vptsChart={<VptsHistChart/>}
-    />
+            {/* Top GRID */}
+            <div 
+              className={`
+                lg:h-full lg:w-full 
+                lg:row-span-[4fr] row-span-1
+
+                flex flex-col
+                lg:grid lg:grid-cols-5 gap-2
+              `}
+            >
+
+              {/* Map */}
+              <div 
+                className={`
+                  lg:w-full lg:h-full h-[60vh]
+                  lg:col-span-4
+                `}
+              >
+                  <HistoryMap/>
+              </div>
+
+              {/* VP chart */}
+              <div 
+                className={`
+                  lg:w-full lg:h-full 
+                  lg:col-span-1 
+                `}
+              >
+                <VpHistChart/>
+              </div>
+
+            </div>
+
+            {/* Bottom GRID */}
+            <div 
+              className={`
+                lg:h-full lg:w-full 
+                lg:row-span-[2fr] row-span-1
+
+                flex flex-col 
+                lg:grid lg:grid-cols-2 gap-2
+              `}
+            >
+
+              {/* Vtip chart */}
+              <div className={`
+                lg:w-full lg:h-full
+                lg:col-span-1
+                `}
+              >
+                <VtipHistChart/>
+              </div>
+
+              {/* Vpts Chart */}
+              <div className={`
+                lg:w-full lg:h-full 
+                lg:col-span-1
+                `}>
+                <VptsHistChart/>
+              </div>
+
+            </div>
+
+
+      </div>
+
+    </MainLayout>
   )
 }
 
--- src/pages/OverviewDashboard.tsx
+++ src/pages/OverviewDashboard.tsx
@@ -2,19 +2,89 @@ import LiveMap from "../features/livemap/LiveMap"
 import VpChart from "../features/vp_chart/VpChart"
 import VptsChart from "../features/vpts_chart/VptsChart"
 import VtipChart from "../features/vtip_chart/VtipChart"
-import DashboardLayout from "../shared/layouts/DashboardLayout"
+import MainLayout from "../shared/layouts/MainLayout"
 
 
 const OverviewDashboard = () => {
   return (
-    <DashboardLayout
+    <MainLayout className="w-full min-h-screen">
+      
+      <div 
+        className={`
+          lg:h-full lg:w-full
+          lg:px-4 lg:py-8 py-4
+          flex flex-col  
+          lg:grid lg:grid-rows-[4fr_2fr] gap-2  
+        `}
+      >
 
-      map={<LiveMap drawable={false} enableLineDraw={false}/>}
-      vpChart={<VpChart/>}
-      vtipChart={<VtipChart/>}
-      vptsChart={<VptsChart/>}
+            {/* Top GRID */}
+            <div 
+              className={`
+                lg:h-full lg:w-full 
+                lg:row-span-[4fr] row-span-1
 
-    />
+                flex flex-col
+                lg:grid lg:grid-cols-5 gap-2
+              `}
+            >
+
+              {/* Map */}
+              <div 
+                className={`
+                  lg:w-full lg:h-full h-[60vh]
+                  lg:col-span-4
+                `}
+              >
+                  <LiveMap drawable={false} enableLineDraw={false}/>
+              </div>
+
+              {/* VP chart */}
+              <div 
+                className={`
+                  lg:w-full lg:h-full 
+                  lg:col-span-1 
+                `}
+              >
+                <VpChart/>
+              </div>
+
+            </div>
+
+            {/* Bottom GRID */}
+            <div 
+              className={`
+                lg:h-full lg:w-full 
+                lg:row-span-[2fr] row-span-1
+
+                flex flex-col 
+                lg:grid lg:grid-cols-2 gap-2
+              `}
+            >
+
+              {/* Vtip chart */}
+              <div className={`
+                lg:w-full lg:h-full
+                lg:col-span-1
+                `}
+              >
+                <VtipChart/>
+              </div>
+
+              {/* Vpts Chart */}
+              <div className={`
+                lg:w-full lg:h-full 
+                lg:col-span-1
+                `}>
+                <VptsChart/>
+              </div>
+
+            </div>
+
+
+      </div>
+
+    </MainLayout>
   )
 }
 
